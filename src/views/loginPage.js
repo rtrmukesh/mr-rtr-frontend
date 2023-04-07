a597907de1b2ba0f6a40806cf58188cbcd57fa4b
Mukesh-Thidiff <mukesh.murugaiyan@thidiff.com>
2023-04-07 20:57:39 +0530

    add google login

--- src/views/loginPage.js
+++ src/views/loginPage.js
@@ -1,23 +1,55 @@
-import React from "react";
-import Form from "./components/Form";
+import { gapi } from "gapi-script";
+import React, { useEffect, useState } from "react";
+import { GoogleLogin, GoogleLogout } from "react-google-login";
+function LoginPage() {
+  
+  const [isLogin, setIsLogin] = useState(false);
 
+  useEffect(() => {
+    function start() {
+      gapi.client.init({
+        clientId:
+          "718337393129-hj31pjdn197dj5p5rlia371p7s04s55v.apps.googleusercontent.com",
+        scope: "email",
+      });
+    }
 
+    gapi.load("client:auth2", start);
+  }, []);
 
+  const onSuccess = (response) => {
+    console.log("SUCCESS", response);
+    if (response.profileObj) {
+      setIsLogin(true);
+    }
+  };
 
+  const onFailure = (response) => {
+    console.log("FAILED", response);
+  };
+  
+  const onLogoutSuccess = () => {
+    console.log("SUCCESS LOG OUT");
+    setIsLogin(false);
+  };
 
-const LoginPage = () => {
-
-    return (
-        <div className="card card-body">
-            <Form
-                initialValues={""}
-                onSubmit
-            >
-            </Form>
-        </div>
-    )
+  return (
+    <div>
+      {!isLogin ? (
+        <GoogleLogin
+          clientId="718337393129-hj31pjdn197dj5p5rlia371p7s04s55v.apps.googleusercontent.com"
+          onSuccess={onSuccess}
+          onFailure={onFailure}
+          theme="dark"
+        />
+      ) : (
+        <GoogleLogout
+          clientId="718337393129-hj31pjdn197dj5p5rlia371p7s04s55v.apps.googleusercontent.com"
+          onLogoutSuccess={onLogoutSuccess}
+        />
+      )}
+    </div>
+  );
 }
-
-
 
 export default LoginPage;
